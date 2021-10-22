d55a23721c3a929230c92f952393da3755d167ae
ettaho <etta0311031@gmail.com>
2021-10-22 17:49:39 +1100

    update gitignore

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
+// Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+//Create an array of questions for user input
+const info = [
+    {
+        name:'title',
+        message:'What is the name of this project?',
+        type: 'input',
+    },
+    {
+        name:'userstory',
+        message:'What is the Userstory of this project?',
+        type: 'input',
+    },
+    {
+        name:'overview',
+        message:'Write a description for your Project.',
+        type: 'input',
+    },
+    {
+        name:'overview',
+        message:'Write a description for your Project.',
+        type: 'input',
+    },
+    
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
